38b2a6f036244f7e92df676a8a9ea7801cf55bcd
pavansantosh-ps <pavansantosh.ips@gmail.com>
2021-09-01 17:08:13 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgxChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,111 @@
+import { ChangeDetectionStrategy, Component, NgZone, OnInit } from '@angular/core';
+import { Color } from '@swimlane/ngx-charts';
+import { timer } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(private zone:NgZone) { }
+  allAlgos = [
+    'Selection Sort',
+    'Bubble Sort',
+    'Insertion Sort',
+    'Merge Sort',
+    'Quick Sort',
+    'Radix Sort'
+  ];
+  selectedAlgo;
+  numberOfElements;
+  saleData = [];
+  customColor = [];
+  mintime = 100;
+  minsize = 10;
+
+  ngOnInit(): void {
+    this.saleData = this.getRandomList(this.minsize);
+    this.customColor = this.getColor([...this.saleData]);
+  }
+
+  getColor(saleData:any){
+    return saleData.map((v,i)=> {
+      return {name: i + '', value: '#0F5298'}
+    })
+  }
+
+  getRandomList(max:number) {
+    let temp = Array.from({length: max}, () => Math.floor(Math.random() * max));
+    return temp.map((v,i) =>  {
+      return {name: i + '', value :v}
+    })
+  }
+
+  submit(){
+    this.selectionSort(this.saleData);
+  }
+
+  async selectionSort(saleData: any[]) {
+    var i, j, minimum_index;
+    const n = saleData.length;
+
+    for (i = 0; i < n-1; i++)
+    {
+        minimum_index = i;
+        for (j = i + 1; j < n; j++){
+          this.changeColor(saleData[i]["name"]);
+          this.changeColor(saleData[j]["name"]);
+          this.customColor = [...this.customColor];
+          await this.wait(this.mintime);
+          if (saleData[j]["value"] < saleData[minimum_index]["value"]){
+            minimum_index = j;
+          }
+          await this.wait(this.mintime);
+          this.changeColor(saleData[i]["name"], '#0F5298');
+          this.changeColor(saleData[j]["name"], '#0F5298');
+          this.customColor = [...this.customColor];
+        }
+        if(i!=minimum_index){
+          this.swap(i, minimum_index);
+          await this.wait(2*this.mintime);
+        }
+    }
+  }
+
+  async swap(i,j){
+    let temp = this.saleData;
+    this.changeColor(temp[i]["name"]);
+    this.changeColor(temp[j]["name"]);
+    this.customColor = [...this.customColor];
+    await this.wait(this.mintime);
+    let temp1 = temp[i]["value"];
+    temp[i]["value"] = temp[j]["value"];
+    temp[j]["value"] =temp1;
+    this.saleData = [...temp];
+    await this.wait(this.mintime);
+    this.changeColor(temp[i]["name"], '#0F5298');
+    this.changeColor(temp[j]["name"], '#0F5298');
+    this.customColor = [...this.customColor];
+  }
+
+  wait(ms) {
+    return timer(ms).pipe(take(1)).toPromise();
+  }
+
+  changeColor(name, color = 'red') {
+    let temp = this.customColor;
+    let elementWithName = null;
+    for(let ele of this.customColor) {
+      if(ele?.name == name) {
+        elementWithName = ele;
+      }
+    }
+    if(elementWithName) {
+      elementWithName.value = color;
+    }
+  }
+}
