e2dc260d25ecb93d68d55dc4342b1f1d47b00560
pavansantosh-ps <pavansantosh.ips@gmail.com>
2021-10-14 15:46:00 +0530

    Auto-generated commit [ci skip]

---
+++ scripts.6593895a4d72efc2e230.js
@@ -0,0 +1,1 @@
+!function(){var a=localStorage["dark-mode"],r=!1;if(null===a)r=window.matchMedia("(prefers-color-scheme: dark)").matches;else try{r=JSON.parse(a).darkMode}catch(e){return console.warn("Error initializing angular-dark-mode"),void console.warn(e)}document.body.classList.add("dark-mode-preloading",r?"dark-mode":"light-mode")}();
